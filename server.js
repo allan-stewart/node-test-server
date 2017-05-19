0b63120f635d9506188c5d6f0f43349b95fdae93
Allan Stewart <stewart.allan@gmail.com>
2017-05-18 22:33:12 -0600

    Rewrite to new JS style

--- server.js
+++ server.js
@@ -1,82 +1,78 @@
-var http = require('http');
-var port = process.argv[2] || 9000;
-var handlers = [];
+const http = require('http')
+const port = process.argv[2] || 9000
+let handlers = []
 
-http.createServer(function (request, response) {
-  var match = handlers.find(function (x) {
-    return x.pattern.test(request.url)
-  });
+http.createServer((request, response) => {
+  let match = handlers.find(x => x.pattern.test(request.url))
   if (match) {
-    match.handler(request, response, request.url.match(match.pattern));
+    match.handler(request, response, request.url.match(match.pattern))
   } else {
-    response.writeHead(404);
-    response.end();
+    response.writeHead(404)
+    response.end()
   }
-}).listen(port, '127.0.0.1');
-console.log('Server running at http://127.0.0.1:' + port);
+}).listen(port, '127.0.0.1')
+console.log('Server running at http://127.0.0.1:' + port)
 
 handlers.push({
   pattern: /^\/echo\/(\d{3})$/i,
-  handler: function (request, response, matches) {
-    var responseBody = '';
-    request.on('data', function (chunk) {
-      responseBody += chunk.toString();
-    });
-    request.on('end', function () {
-      response.setHeader('x-method', request.method);
+  handler: (request, response, matches) => {
+    let responseBody = ''
+    request.on('data', chunk => responseBody += chunk.toString())
+    request.on('end', () => {
+      response.setHeader('x-method', request.method)
       if (request.headers['content-type']) {
-        response.setHeader('content-type', request.headers['content-type']);
+        response.setHeader('content-type', request.headers['content-type'])
       }
-      response.writeHead(parseInt(matches[1], 10));
+      response.writeHead(parseInt(matches[1], 10))
       if (responseBody.length > 0) {
-        response.write(responseBody);
+        response.write(responseBody)
       }
-      response.end();
-    });
+      response.end()
+    })
   }
-});
+})
 
 handlers.push({
   pattern: /^\/headers$/i,
-  handler: function (request, response, matches) {
-    response.writeHead(200, {'content-type': 'application/json'});
-    response.end(JSON.stringify(request.headers));
+  handler: (request, response, matches) => {
+    response.writeHead(200, {'content-type': 'application/json'})
+    response.end(JSON.stringify(request.headers))
   }
-});
+})
 
 handlers.push({
   pattern: /^\/delay\/(\d+)$/i,
-  handler: function (request, response, matches) {
-    var delay = parseInt(matches[1], 10);
-    setTimeout(function () {
-      response.writeHead(200, {'content-type': 'text/plain'});
-      response.end('Delayed for ' + delay);
-    }, delay * 1000);
+  handler: (request, response, matches) => {
+    let delay = parseInt(matches[1], 10)
+    setTimeout(() => {
+      response.writeHead(200, {'content-type': 'text/plain'})
+      response.end('Delayed for ' + delay)
+    }, delay * 1000)
   }
-});
+})
 
 handlers.push({
   pattern: /^\/stream\/(\d+)$/i,
-  handler: function (request, response, matches) {
-    var delaySeconds = parseInt(matches[1], 10);
-    var counter = 0;
-    var interval = setInterval(function () {
-      counter++;
-      response.write(counter + '\n');
+  handler: (request, response, matches) => {
+    let delaySeconds = parseInt(matches[1], 10);
+    let counter = 0;
+    let interval = setInterval(() => {
+      counter++
+      response.write(counter + '\n')
       if (counter >= delaySeconds) {
-        clearInterval(interval);
-        response.end();
+        clearInterval(interval)
+        response.end()
       }
-    }, 1000);
-    response.writeHead(200, {'content-type': 'text/plain'});
+    }, 1000)
+    response.writeHead(200, {'content-type': 'text/plain'})
   }
-});
+})
 
 handlers.push({
   pattern: /^\/cookies$/i,
-  handler: function (request, response, matches) {
+  handler: (request, response, matches) => {
     response.setHeader('set-cookie', ['alpha=1', 'beta=2'])
-    response.writeHead(200);
-    response.end();
+    response.writeHead(200)
+    response.end()
   }
-});
+})
